533d760cd521892da3201c7b9935a8a5c9bb3437
rbernanda <acappemenang@gmail.com>
2021-06-02 20:06:23 +0700

    Added static layout on listview

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Contact List',
       theme: ThemeData(
-        primarySwatch: Colors.green.shade400,
+        primarySwatch: Colors.green,
       ),
-      home: ContactPage(title: 'Contact List'),
+      home: ContactPage(title: 'Contacts'),
     );
   }
 }
@@ -31,19 +31,137 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
-  
+  Future<List<User>> _getUsers() async {
+    var uri = Uri.parse('https://reqres.in/api/users?per_page=12');
+    var response = await http.get(uri);
+
+    List<User> users = [];
+    if (response.statusCode == 200) {
+      Map<String, dynamic> map = convert.jsonDecode(response.body);
+      List<dynamic> data = map["data"];
+      print(data[0]["first_name"]);
+      for (var u in data) {
+        User user = User(u["id"], u["email"],
+            u["first_name"] + " " + u["last_name"], u["avatar"]);
+        users.add(user);
+      }
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+
+    return users;
+  }
+
+  int _selectedIndex = -1;
+
+  Color _getSelectedTextColor(int index) {
+    return _selectedIndex == index ? Colors.white : Colors.black;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        centerTitle: true,
       ),
       body: Container(
-        child: Center(
-          child: Text('hello world')
-        )
+        child: FutureBuilder(
+          future: _getUsers(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return Container(
+                child: Center(
+                  child: Text('Loading'),
+                ),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 100.0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      textDirection: TextDirection.ltr,
+                      children: <Widget>[
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.25,
+                          height: 100.0,
+                          child: FittedBox(
+                            child: Image.network(snapshot.data[index].avatar,
+                                fit: BoxFit.cover),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        Container(
+                            width: MediaQuery.of(context).size.width * 0.75,
+                            height: 100.0,
+                            padding: EdgeInsets.only(left: 5),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              textDirection: TextDirection.ltr,
+                              children: <Widget>[
+                                Container(
+                                  padding: EdgeInsets.only(left: 20),
+                                  child: Center(
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      textDirection: TextDirection.ltr,
+                                      children: <Widget>[
+                                        Text(
+                                          snapshot.data[index].fullName,
+                                          style: TextStyle(
+                                              color:
+                                                  _getSelectedTextColor(index),
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18.0),
+                                        ),
+                                        SizedBox(height: 5),
+                                        Text(
+                                          snapshot.data[index].email,
+                                          style: TextStyle(
+                                              color:
+                                                  _getSelectedTextColor(index),
+                                              fontSize: 14.0),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.only(right: 20),
+                                  child: Center(
+                                    child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        textDirection: TextDirection.ltr,
+                                        children: <Widget>[
+                                          Icon(Icons.phone, color: Colors.black,),
+                                          SizedBox(width: 15),
+                                          Icon(Icons.chat_bubble_outline_outlined, color: Colors.black,)
+                                        ],
+                                        ),
+                                  ),
+                                )
+                              ],
+                            ))
+                      ],
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
@@ -53,3 +171,49 @@ class _ContactPageState extends State<ContactPage> {
     );
   }
 }
+
+class User {
+  final int id;
+  final String email;
+  final String fullName;
+  final String avatar;
+
+  User(this.id, this.email, this.fullName, this.avatar);
+}
+
+// return ListTile(
+//                     leading: ConstrainedBox(
+//                         constraints: BoxConstraints(
+//                           minWidth: 44,
+//                           minHeight: 100,
+//                           maxWidth: 64,
+//                           maxHeight: 100,
+//                         ),
+
+//                         child: Image.network(snapshot.data[index].avatar, fit: BoxFit.cover)),
+//                     title: Text(
+//                       snapshot.data[index].fullName,
+//                       style: TextStyle(
+//                           color: _getSelectedTextColor(index),
+//                           fontWeight: FontWeight.bold,
+//                           fontSize: 18.0),
+//                     ),
+//                     subtitle: Text(snapshot.data[index].email,
+//                         style: TextStyle(
+//                             color: _getSelectedTextColor(index),
+//                             fontSize: 14.0)),
+//                             contentPadding:
+//                         EdgeInsets.all(0),
+//                     // contentPadding:
+//                     //     EdgeInsets.symmetric(vertical: 20.0, horizontal: 16.0),
+//                     tileColor: index.toInt() % 2 != 0
+//                         ? Colors.grey[100]
+//                         : Colors.white,
+                    // onTap: () {
+                    //   setState(() {
+                    //     _selectedIndex = index;
+                    //   });
+                    // },
+//                     selected: index == _selectedIndex,
+//                     selectedTileColor: Colors.purple[100],
+//                   );
